35ef8010ce5de5ae7e486538e0a39beac6754497
thisisrohit01 <your-thisisrohit01.com>
2026-06-06 14:52:12 +0530

    day 1

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version.
+
+let name="rohit kumar singh";
+let age=24;
+let isLoggedIn=false
+let city=''
+console.log(city)
+
+
+//number range 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null=standalone value
+//undefined
+//symbol=> unique
+
+
+//object
+
+console.log(typeof(name))// tells the datatypw
+console.log(typeof(null))//null is an object
+console.log(typeof(undefined))
