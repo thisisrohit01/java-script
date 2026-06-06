47336bc7a7e6aeb89390f2fcf36c20a23a19547a
thisisrohit01 <your-thisisrohit01.com>
2026-06-06 14:53:22 +0530

    day 1

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+const accountId=24001390238
+let userName="Rohit kumar singh"
+var accountPassword="123456"
+city="kanpur"
+
+console.log(accountId)
+console.table([userName,accountPassword,city])
+
