f60a535eb901240616efce45689460822d313ee8
thisisrohit01 <your-thisisrohit01.com>
2026-06-07 12:10:14 +0530

    organizing the files

---
+++ basic101/conversionOperation.js
@@ -0,0 +1,60 @@
+// let score="66"
+// let age="twenty"
+// let temp=null
+//
+//
+// console.log(typeof score)
+// console.log(typeof (age))
+// console.log(typeof (temp))
+//
+// let valueInNumber=Number(score)//Number starts with capital
+// let ageInNumber=Number(age)//what happens if we convert something which is not a number
+// let tempInNumber=Number(temp);
+//
+// console.log(valueInNumber)
+// console.log(ageInNumber)
+// console.log(tempInNumber)
+//
+// //33 => 33
+// //33a => NaN
+// //true => 1
+//
+// let isLoggedIn=undefined;
+//
+// let isLoggedInbool=Boolean(isLoggedIn)
+// console.log(isLoggedInbool)
+
+// "" empty string & 0 will give false null and undefined also
+// any positive or negative number or any string will give true
+
+// **************************Operations****************************
+
+// let value=3
+// let negativeValue=-(value)
+// console.log(negativeValue)
+//
+// console.log(3+2)
+// console.log(2**3)
+// console.log(9-3*3)
+// console.log(3%2)
+//
+// let str1="rohit"
+// let str2="kumar"
+// let str3=str1+' '+str2
+// console.log(str3)
+// console.log(str1+' '+str2+' '+"singh")
+
+// console.log(1+"2") //both are treated as string here
+//
+// let gameCounter=100;
+// console.log(gameCounter++)
+// console.log(gameCounter)
+// console.log(++gameCounter)
+
+// ******************************Comparison************************
+// checks for equality
+/// strictly checks for equality along wiht the data types
+console.log("2"==2)//this converts the value and then checks
+console.log("3"===3)
+console.log(null==0)
+console.log(null>=0)
