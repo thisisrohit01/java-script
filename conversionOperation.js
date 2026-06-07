4798d82b719714eb99e89a62cae8af735a8e8a00
thisisrohit01 <your-thisisrohit01.com>
2026-06-07 11:18:58 +0530

    day 2

---
+++ conversionOperation.js
@@ -0,0 +1,31 @@
+let score="66"
+let age="twenty"
+let temp=null
+
+
+console.log(typeof score)
+console.log(typeof (age))
+console.log(typeof (temp))
+
+let valueInNumber=Number(score)//Number starts with capital
+let ageInNumber=Number(age)//what happens if we convert something which is not a number
+let tempInNumber=Number(temp);
+
+console.log(valueInNumber)
+console.log(ageInNumber)
+console.log(tempInNumber)
+
+//33 => 33
+//33a => NaN
+//true => 1
+
+let isLoggedIn=undefined;
+
+let isLoggedInbool=Boolean(isLoggedIn)
+console.log(isLoggedInbool)
+
+// "" empty string & 0 will give false null and undefined also
+// any positive or negative number or any string will give true
+
+// **************************Operations****************************
+
